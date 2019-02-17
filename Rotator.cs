855e927b50908a365115ac53b97122f856569377
ingofreak <33750816+ingofreak@users.noreply.github.com>
2019-02-17 19:25:28 +0000

    Update Rotator.cs

--- Rotator.cs
+++ Rotator.cs
@@ -3,12 +3,10 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
-	// Before rendering each frame..
+	
 	void Update () 
 	{
-		// Rotate the game object that this script is attached to by 15 in the X axis,
-		// 30 in the Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it per second
-		// rather than per frame.
+		// þetta er eitthvað sem er í roll-a-ball. Gleymdi að taka þetta út.
 		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
 	}
 }	
